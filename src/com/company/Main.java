59a1fd3279c5d26edb6750562c3821cfc5a7bf10
Kostyan2018 <aub2011@i.ua>
2018-02-22 09:43:34 +0200

    lb2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,19 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         String separator = File.separator;
-        String path = separator +"G:" + separator + "lb1.txt";
+        String path = separator + '.' + separator + "lb1.txt";
 
         File file = new File(path);
-
         //File file = new File("G:\\lb1.txt");
 
         Scanner scanner = new Scanner(file);
@@ -22,7 +25,12 @@ public class Main {
             //x
             //,
             //y
-            System.out.println(row);
+            //System.out.println(row);
+            Pattern pattern = Pattern.compile(" |;|,");
+            // спитати , чи є кращі параметри для розділення Pattern pattern = Pattern.compile(" ??? ");
+            String[] lexems;
+            lexems = pattern.split(row);
+            Arrays.asList(lexems).forEach(lexem -> System.out.println(lexem) );
         }
         scanner.close();
     }
